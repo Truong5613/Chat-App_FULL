0889ffed0bbd332b98f3d9d7ae7dc47d0e9c26de
Truong5613 <143669324+Truong5613@users.noreply.github.com>
2024-10-13 00:12:47 +0700

    Emoji Chat

--- Chat App server/src/model/Model_Send_Message.java
+++ Chat App server/src/model/Model_Send_Message.java
@@ -11,10 +11,19 @@ package model;
  * @author mrtru
  */
 public class Model_Send_Message {
+    private int messageType;
     private int fromUserID;
     private int toUserID;
     private String text;
     
+    public int getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(int messageType) {
+        this.messageType = messageType;
+    }
+
     public int getFromUserID() {
         return fromUserID;
     }
@@ -38,8 +47,9 @@ public class Model_Send_Message {
     public void setText(String text) {
         this.text = text;
     }
-    
-    public Model_Send_Message(int fromUserID, int toUserID, String text) {
+
+    public Model_Send_Message(int messageType, int fromUserID, int toUserID, String text) {
+        this.messageType = messageType;
         this.fromUserID = fromUserID;
         this.toUserID = toUserID;
         this.text = text;
@@ -47,6 +57,5 @@ public class Model_Send_Message {
 
     public Model_Send_Message() {
     }
-    
 
 }
