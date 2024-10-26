4428cd3c0d3304d99b25f2ef1396d54fb02b0bb1
Truong5613 <143669324+Truong5613@users.noreply.github.com>
2024-10-27 01:02:29 +0700

    them sql

--- SQL/chat_app.sql
+++ SQL/chat_app.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `chat_application` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `chat_application`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: chat_application
@@ -18,6 +16,38 @@ USE `chat_application`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `box_chat`
+--
+
+DROP TABLE IF EXISTS `box_chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `box_chat` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `image` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `box_chat_members`
+--
+
+DROP TABLE IF EXISTS `box_chat_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `box_chat_members` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `box_chat_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `box_chat_id` (`box_chat_id`),
+  CONSTRAINT `box_chat_members_ibfk_1` FOREIGN KEY (`box_chat_id`) REFERENCES `box_chat` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `files`
 --
 
@@ -31,7 +61,7 @@ CREATE TABLE `files` (
   `BlurHash` varchar(255) DEFAULT NULL,
   `Status` char(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`FileID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,14 +79,11 @@ CREATE TABLE `message` (
   `Text` text,
   `Time` timestamp NULL DEFAULT NULL COMMENT '\\n\\n',
   `FileID` int unsigned DEFAULT NULL,
+  `BoxId` int DEFAULT NULL,
   PRIMARY KEY (`MessageID`),
-  KEY `FK_FromUser` (`FromUserID`),
-  KEY `FK_ToUser` (`ToUserID`),
   KEY `FK_File` (`FileID`),
-  CONSTRAINT `FK_File` FOREIGN KEY (`FileID`) REFERENCES `files` (`FileID`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `FK_FromUser` FOREIGN KEY (`FromUserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_ToUser` FOREIGN KEY (`ToUserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `FK_File` FOREIGN KEY (`FileID`) REFERENCES `files` (`FileID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +98,7 @@ CREATE TABLE `user` (
   `UserName` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,15 +109,19 @@ DROP TABLE IF EXISTS `user_account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_account` (
-  `UserID` int NOT NULL,
-  `UserName` varchar(255) NOT NULL,
-  `Gender` char(1) NOT NULL DEFAULT '',
+  `UserID` int unsigned NOT NULL,
+  `UserName` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Gender` char(1) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `Image` longblob,
-  `ImageString` varchar(255) DEFAULT ' ',
-  `Status` char(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`UserID`),
-  CONSTRAINT `UserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ImageString` longtext COLLATE utf8mb4_general_ci,
+  `ImageBackground` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ImageBackgroundString` longtext COLLATE utf8mb4_general_ci,
+  `Birthday` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `Address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `Description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `Status` char(1) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1',
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -102,4 +133,4 @@ CREATE TABLE `user_account` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-20 23:00:41
+-- Dump completed on 2024-10-27  1:01:40
